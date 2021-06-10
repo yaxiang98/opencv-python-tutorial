85ea8f8f690531c629240618b8a0e99868dbdfdb
yaxiang98 <shawn8583@163.com>
2021-06-10 22:42:16 +0800

    morphological_transformation

--- scripts/17_morphological_transformations.py
+++ scripts/17_morphological_transformations.py
@@ -13,5 +13,22 @@ dilation = cv2.dilate(mask, kernal, iterations=2)
 erosion = cv2.erode(mask, kernal, iterations=5)
 # erosion method applies kernal to each pixel and only when
 # all pixels are 1, the pixel applied will be 1.
+openning = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernal)
 
+closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
 
+morph_gradient = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
+closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
+
+# kernal size of 2x2 and iterations = 1 will be the best
+# performance in this picture.
+
+titles = ['image', 'mask', 'dilation', 'erosion', 'openning']
+images = [img, mask, dilation, erosion]
+
+for i in range(4):
+    plt.subplot(2, 2, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
